d0c0e4094dfa98dfce20de6e745d50024f718e2e
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2025-05-13 16:43:04 +0200

    Initial commit

---
+++ packwiz/pack_content.py
@@ -0,0 +1,64 @@
+# if none are set, everything is included
+# include: ONLY use this project for these versions
+# exclude: use this project for all versions EXCEPT for these
+# include and exclude can't be used at the same time
+#
+# force-versions: force download this modrinth url version for speicific mc version (see example for sodium)
+#
+# For an example pack_content.py file, see https://github.com/Thijzert123/client-plus/blob/main/packwiz/pack_content.py
+
+{
+    "mods": [
+        {"id": "yosbr",
+            "force-versions": {
+                "1.21.5": "KMOzdYko"
+        }},
+        {"id": "moreculling"},
+        {"id": "modmenu"},
+        {"id": "iris"},
+        {"id": "dynamic-fps"},
+        {"id": "sodium"},
+        {"id": "zoomify"},
+        {"id": "ferrite-core"},
+        {"id": "sodium-extra"},
+        {"id": "immediatelyfast"},
+        {"id": "lambdynamiclights"},
+        {"id": "badoptimizations"},
+        {"id": "optigui"},
+        {"id": "better-stats"},
+        {"id": "simple-voice-chat"},
+        {"id": "chat-heads"},
+        {"id": "mouse-tweaks"},
+        {"id": "clientsort"},
+        {"id": "modelfix"},
+        {"id": "entityculling"},
+        {"id": "lithium"},
+        {"id": "appleskin"},
+        {"id": "cubes-without-borders"},
+        {"id": "continuity"},
+        {"id": "noisium"},
+        {"id": "shulkerboxtooltip"},
+        {"id": "3dskinlayers"},
+        {"id": "xaeros-world-map"},
+        {"id": "xaeros-minimap"},
+        {"id": "clumps"},
+        {"id": "improved-sign-editing"},
+        {"id": "craftpresence"},
+        {"id": "krypton"},
+        {"id": "gamma-utils"},
+        {"id": "reeses-sodium-options"},
+        {"id": "fast-trading"},
+        {"id": "emotecraft"},
+        {"id": "scribble"},
+        {"id": "symbol-chat"},
+        {"id": "mod-loading-screen"},
+        {"id": "no-resource-pack-warnings"}],
+    "shaderpacks": [],
+    "resourcepacks": [
+        {"id": "redstone-tweaks",
+            "force-versions": {
+                "1.21.5": "G3HClwKQ"
+        }},
+        {"id": "recolourful-containers-gui"}
+    ]
+}
